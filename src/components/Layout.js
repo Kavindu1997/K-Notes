592e186db2a2c45efa5688b9d3f83f403b82aeeb
Kavindu1997 <kksamaraweera1997@gmail.com>
2021-05-30 17:17:06 +0530

    permanent drawer added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,17 +1,39 @@
-import { makeStyles } from '@material-ui/core'
+import { Drawer, makeStyles, Typography } from '@material-ui/core'
 import React from 'react'
 
+const drawerWidth=240
 const useStyles=makeStyles({
     page:{
         background:'whitesmoke',
         width:'100%'
+    },
+    drawer:{
+        width:drawerWidth
+    },
+    drawerPaper:{
+        width:drawerWidth
+    },
+    root:{
+        display:'flex'
     }
 })
 export default function Layout({children}){
     const classes=useStyles()
 
     return(
-      <div>
+      <div className={classes.root}>
+          <Drawer
+            className={classes.drawer}
+            variant="permanent"
+            anchor="left"
+            classes={{paper:classes.drawerPaper}}
+          >
+              <div>
+                  <Typography variant="h5">
+                       K-Notes
+                  </Typography>
+              </div>
+          </Drawer>
             <div className={classes.page}>
                 {children}
             </div>
